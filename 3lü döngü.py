a39759c1cab5c7d445f1672880704651da45ae90
burak turhan <110310536+BurakT123@users.noreply.github.com>
2023-11-09 01:02:18 +0300

    Delete 3lü döngü.py

--- 3lü döngü.py
+++
@@ -1,7 +0,0 @@
-for c in range(6,3,-2):
-        for b in range(4,8,+3):
-            for a in range(3,6,+1):
-                    print(b,a,c)
-
-
-""""İç içe döngülerde kural en fazla döneni en içe yani en sona ,en az döneni en dışa yani en başa"""
